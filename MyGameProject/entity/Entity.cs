22226c97351c1212755ad38191b289cf46299e9e
Erkman Geliş <88348596+erkmangelis@users.noreply.github.com>
2024-01-31 10:19:34 +0300

    Add files via upload

---
+++ MyGameProject/entity/Entity.cs
@@ -0,0 +1,14 @@
+namespace MyGameProject
+{
+
+    public class Entity
+    {
+        public int x, y;
+        public int baseSpeed;
+
+        public Bitmap up1, up2, down1, down2, left1, left2, right1, right2;
+        public string direction;
+        public int spriteNumber;
+        public int spriteCounter;
+    }
+}
---
+++ MyGameProject/entity/Player.cs
@@ -0,0 +1,141 @@
+namespace MyGameProject
+{
+    public class Player : Entity
+    {
+        GamePanel gamePanel;
+        KeyHandler keyH;
+
+
+        // Initialization
+        public Player(GamePanel gameP, KeyHandler keyH)
+        {
+            this.gamePanel = gameP;
+            this.keyH = keyH;
+
+            setDefaultValues();
+            getPlayerSprite();
+        }
+
+
+        // Setting default player values
+        public void setDefaultValues()
+        {
+            x = 200;
+            y = 200;
+            baseSpeed = 4;
+            direction = "down";
+        }
+
+
+        // Gets player sprite from assets folder
+        public void getPlayerSprite()
+        {
+            try
+            {
+                up1 = new Bitmap("./assets/player/up1.png");
+                up2 = new Bitmap("./assets/player/up2.png");
+                down1 = new Bitmap("./assets/player/down1.png");
+                down2 = new Bitmap("./assets/player/down2.png");
+                left1 = new Bitmap("./assets/player/left1.png");
+                left2 = new Bitmap("./assets/player/left2.png");
+                right1 = new Bitmap("./assets/player/right1.png");
+                right2 = new Bitmap("./assets/player/right2.png");
+            }
+
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+
+        // Update player
+        public void update()
+        {
+            // Only increase the spriteCounter while the player is moving
+            if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed)
+            {
+                spriteCounter++;
+            }
+
+
+            int speed;
+            // Speed calibration for the diagonal movement
+            if (keyH.DiagonalMovement())
+            {
+                speed = (int)Math.Round(baseSpeed * 0.707);
+            }
+            else
+            {
+                speed = baseSpeed;
+            }
+
+
+            // Vertical Movement
+            if (keyH.upPressed)
+            {
+                direction = "up";
+                y -= speed;
+            }
+            else if (keyH.downPressed)
+            {
+                direction = "down";
+                y += speed;
+            }
+
+
+            // Horizontal Movement
+            if (keyH.leftPressed)
+            {
+                direction = "left";
+                x -= speed;
+            }
+            else if (keyH.rightPressed)
+            {
+                direction = "right";
+                x += speed;
+            }
+
+
+            // Iterate the sprite -> 60 / 12 times per second
+            if (spriteCounter > 12)
+            {
+                spriteNumber = (spriteNumber == 1) ? 2 : 1;
+                spriteCounter = 0;
+            }
+        }
+
+        
+        
+        // Draw player
+        public void draw(Graphics graphics)
+        {
+            Bitmap sprite = down1;
+
+
+            // Change sprite according to direction
+            switch (direction)
+            {
+                case "up":
+                    sprite = (spriteNumber == 1) ? up1 : up2;
+                    break;
+
+                case "down":
+                    sprite = (spriteNumber == 1) ? down1 : down2;
+                    break;
+
+                case "left":
+                    sprite = (spriteNumber == 1) ? left1 : left2;
+                    break;
+
+                case "right":
+                    sprite = (spriteNumber == 1) ? right1 : right2;
+                    break;
+            }
+
+            
+            // Draw player sprite
+            graphics.DrawImage(sprite, new Rectangle (x, y, Config.tileSize, Config.tileSize));
+        }
+    }
+}
